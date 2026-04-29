ccf3043040f318e60327f782ed865fe7ebedb3c2
Yashita Aggarwal <yashitaaggarwal.2815@gmail.com>
2026-04-29 17:48:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/carbon-footprint-tracking">
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
